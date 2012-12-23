d23320896f128a267cbb908a7278ea2f191e1b22
Josh Duffy <jduffy@mines.edu>
2012-12-23 15:52:46 -0700

    Created parent signout

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -38,6 +38,11 @@ end
       it { should have_link('Profile', href: parent_path(parent)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
+
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
     end
   end
 end
