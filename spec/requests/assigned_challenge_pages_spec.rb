f89553ac86255901efdad98b4374bdc75408741c
Josh Duffy <jduffy@mines.edu>
2013-01-19 08:10:24 -0700

    assign chall pg spec choose not working

--- spec/requests/assigned_challenge_pages_spec.rb
+++ spec/requests/assigned_challenge_pages_spec.rb
@@ -6,15 +6,13 @@ describe "Assigned Challenges" do
 
   describe "assign challenge page" do
     let(:parent) { FactoryGirl.create(:parent) }
-    let(:challenge) { FactoryGirl.create(:challenge, parent_id: parent.id) }
-    let(:child) { FactoryGirl.create(:child, parent_id: parent.id) }
   
     before do
       sign_in parent
-      challenge.save
-      child.save
       visit new_assigned_challenge_path
     end
+
+    let(:submit) { "Assign Challenge" }
 
     describe "should have the right title and page heading" do
       it { should have_title('Assign a Challenge') }
@@ -28,20 +26,47 @@ describe "Assigned Challenges" do
     end
 
     describe "should have the right choices" do
-      it { should have_selector('label', text: challenge.name)  }
+      let(:challenge) { FactoryGirl.create(:challenge, parent_id: parent.id, name: "Test Challenge") }
+      let(:child) { FactoryGirl.create(:child, parent_id: parent.id) }
+      before do
+        challenge.save
+        child.save
+      end
+
+      it { should have_selector(challenge.name)  }
       it { should have_selector('label', text: child.username)  }
+      it { has_field?(challenge.name) }
+      it { has_field?(child.username) }
     end
 
     describe "assignment" do
-      before do
-        #choose challenge.name
-        choose child.username
-        fill_in "Assign Points (9-999)", with: '100'
+
+      describe "with invalid information" do
+        it "should not assign a challenge" do
+          expect { click_button submit }.not_to change(AssignedChallenge, :count)
+        end
       end
+
+      describe "with valid information" do
+        before do
+          #choose(challenge.name)
+          #choose child.username
+          #fill_in "Assign Points (9-999)", with: '100'
+        end
   
-      it "should assign a challenge" do
-        expect { click_button "Assign Challenge" }.to change( AssignedChallenge, :count).by(1)
-      end
+        it "should create a challenge" do
+          expect { click_button submit }.to change( AssignedChallenge, :count).by(1)
+        end   
+  
+        describe "after assigning the challenge" do
+          before { click_button submit }
+          let(:challenge) { Challenge.find_by_name('test challenge') }
+  
+          it { should have_title(challenge.name) }
+          it { should have_h1(challenge.name) }
+          it { should have_success_message('You')}
+        end
+      end 
     end
   end
 end
