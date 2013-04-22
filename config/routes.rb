9571acf8b115a62cd1021483d95f1946308ef27b
Kelton Manzanares <kmanzana@mines.edu>
2013-04-22 14:05:30 -0600

    dash work

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@ Hm::Application.routes.draw do
   match '/rewards_community_pool',    to: 'rewards#community_pool'
   match '/children_your',             to: 'children#your'
   match '/parent_dash',               to: 'parents#dash'
+  match '/completed_challenges',      to: 'assigned_challenges#completed'
 
   match '/contact' => 'contact#new', :as => 'contact', :via => :get
   match '/contact' => 'contact#create', :as => 'contact', :via => :post
--- spec/javascripts/dash_spec.js
+++ spec/javascripts/dash_spec.js
@@ -6,11 +6,12 @@
 describe('DashCtrl', function() {
   var scope, ctrl, $httpBackend;
   var children = [{"age_group":"6-8","avatar":null,"created_at":"2013-02-15T14:30:55Z","id":1,"parent_id":1,"password_digest":"$2a$10$aoTfwANODSAaL6MdIZtCjung39Q89xXTsKFQWxYDmMnT1DvZ3516.","points":1151,"remember_token":"OPrgR7h0f0o5c5R3MQGaLA","updated_at":"2013-03-23T00:09:56Z","username":"my_child"},{"age_group":"8-10","avatar":null,"created_at":"2013-03-01T17:44:13Z","id":2,"parent_id":1,"password_digest":"$2a$10$WQutrxR0b2/uzzgze4165OM3okUZRUkc6b0o3XDtH.vmsn.RNuwP.","points":0,"remember_token":"8E2rUZzrX70g0GEzkUiPiw","updated_at":"2013-03-01T17:44:45Z","username":"another_child"}];
+  var completed_challenges = [{"accepted":false,"category_id":1,"challenge_id":1,"child_id":1,"completed":true,"created_at":"2013-03-24T09:32:26Z","id":7,"parent_id":1,"points":100,"rejected":false,"updated_at":"2013-03-31T05:28:38Z","validated":false}];
 
   beforeEach(inject(function(_$httpBackend_, $rootScope, $controller){
     $httpBackend = _$httpBackend_;
     $httpBackend.expectGET('/children_your.json').respond(children);
-    $httpBackend.expectGET('/completed_challenges.json').respond();
+    $httpBackend.expectGET('/completed_challenges.json').respond(completed_challenges);
 
     scope = $rootScope.$new();
     ctrl = $controller(DashCtrl, {$scope: scope});
@@ -21,6 +22,13 @@ describe('DashCtrl', function() {
     $httpBackend.flush();
 
     expect(scope.children).toEqual(children);
+  });
+
+  it('should create "completed_challenges" model fetched from xhr', function() {
+    expect(scope.completed_challenges).toBeUndefined();
+    $httpBackend.flush();
+
+    expect(scope.completed_challenges).toEqual(completed_challenges);
   });
 
 });
