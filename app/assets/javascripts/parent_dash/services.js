8a300f2fa9b645b43f9429699d48ca6ad1d6ecf3
Kelton Manzanares <kmanzana@mines.edu>
2013-05-24 18:00:28 -0600

    minification fixes

--- app/assets/javascripts/parent_dash/services.js
+++ app/assets/javascripts/parent_dash/services.js
@@ -1,17 +1,17 @@
-healthmonster.factory('Challenge', function($resource) {
+healthmonster.factory('Challenge', ['$resource', function($resource) {
   return $resource('/challenges', {},
     { 'create': { method: 'POST'}})
-})
+}])
 
 healthmonster.factory('Reward', function($resource) {
   return $resource('/rewards', {},
     { 'create': { method: 'POST'}})
 })
 
-healthmonster.factory('ChildrenYour', function($resource) {
+healthmonster.factory('ChildrenYour', ['$resource', function($resource) {
   return $resource('/children_your', {}, 
     { 'index': { method: 'GET', isArray: true}}) 
-})
+}])
 
 healthmonster.factory('CompletedChallenges', function($resource) {
   return $resource('/completed_challenges', {},
@@ -27,6 +27,10 @@ healthmonster.factory('EnabledReward', function($resource) {
   return $resource('/enabled_rewards', {},
     { 'create': { method: 'POST'}})
 })
+
+appModule.factory('sharedApplication', ['$rootScope','$http',function($rootScope, $http) {
+
+}]);
 
 //this works
 /*$http.post('/challenges', data)
