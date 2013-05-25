7cd9ee2c525b18b15887d301bad1d0f64333d465
Kelton Manzanares <kmanzana@mines.edu>
2013-05-24 18:23:15 -0600

    typo

--- app/assets/javascripts/rewards/services.js
+++ app/assets/javascripts/rewards/services.js
@@ -1,7 +1,7 @@
 rewards.factory('Reward', ['$resource', function($resource) {
   return $resource('/rewards', {},
     { 'create': { method: 'POST'}})
-})]
+}])
 
 rewards.factory('RewardsCommunityPool', ['$resource', function($resource) {
   return $resource('/rewards_community_pool', {},
